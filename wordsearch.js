426b5627c8cbc7eda9504c1970a2cb48a4edb1ef
kmunirpm <km092009@gmail.com>
2021-11-08 02:30:48 +0000

    word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
 const wordSearch = (letters, word) => { 
+    if (word === "") return false;
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+    const full = horizontalJoin.concat(verticalJoin);
+
+    for (l of full) {
         if (l.includes(word)) return true
-    }
+    }    
 }
+
+
+const transpose = function(matrix) {
+    for (var i = 0; i < matrix.length; i++) {
+      for (var j = 0; j < i; j++) {
+        [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+      }
+    }
+    return matrix;
+}
+
 
 module.exports = wordSearch
